19085120ed8c25db12cde21fbc5bc352fde6401d
Tieu Long <tieulong99@hotmail.ch>
2022-11-03 17:02:56 +0100

    Correct image size

---
+++ js/image.js
@@ -0,0 +1,27 @@
+const fetchImages = async () => {
+  const response = await fetch("../imageList.txt");
+  const images = await response.text();
+  return images;
+};
+
+async function main() {
+  const images = await fetchImages();
+  const imageList = images.split("\n");
+
+  imageList.forEach((element) => {
+    const elements = element.split(".");
+    const newImage = document.createElement("img");
+    newImage.setAttribute("class", "image");
+    newImage.setAttribute("loading", "lazy");
+    newImage.setAttribute(
+      "alt",
+      elements[0].replace(/_/g, " ")
+    );
+    newImage.setAttribute(
+      "src",
+      "./images/thumbnails/" + elements[0] + "-TN.JPG"
+    );
+    document.getElementById("gallery").append(newImage);
+  });
+}
+main();
